26c736633e4ac02f4345d4c10ba2e2998b99b55a
Siroz555 <noreply.siroz555@gmail.com>
2025-07-25 21:46:04 +0200

    Backend: NetworkManager Update
    Send the PingPacket every 5s, without only work on SkyBlock

--- src/main/java/fr/siroz/cariboustonks/manager/network/NetworkManager.java
+++ src/main/java/fr/siroz/cariboustonks/manager/network/NetworkManager.java
@@ -1,8 +1,11 @@
 package fr.siroz.cariboustonks.manager.network;
 
+import fr.siroz.cariboustonks.core.scheduler.TickScheduler;
+import fr.siroz.cariboustonks.event.EventHandler;
 import fr.siroz.cariboustonks.event.NetworkEvents;
 import fr.siroz.cariboustonks.manager.Manager;
 import fr.siroz.cariboustonks.util.math.MathUtils;
+import java.util.concurrent.TimeUnit;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.c2s.query.QueryPingC2SPacket;
 import net.minecraft.util.Util;
@@ -15,7 +18,7 @@ public final class NetworkManager implements Manager {
 	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
 
 	private long lastPingResult = 0;
-	private long displayedPing = 0;
+	private long currentPingResult = 0;
 	private long lastUpdateTime = 0;
 
 	private final float[] tickRates = new float[20];
@@ -28,34 +31,23 @@ public final class NetworkManager implements Manager {
 		NetworkEvents.PING_RESULT.register(ping -> this.lastPingResult = ping);
 		NetworkEvents.WORLD_TIME_UPDATE_PACKET.register(this::onWorldTimeUpdatePacket);
 		NetworkEvents.GAME_JOIN_PACKET.register(this::onGameJoinPacket);
-
-		// TODO
-		//  J'ai plus besoin d'envoyer un packet ?
-		//  Je crois que Skyblocker le fait déjà, donc techniquement pas besoin de re-envoyer.
-		//  À vérifier
-
-		//TickScheduler.getInstance().runRepeating(this::sendPingPacket, 5, TimeUnit.SECONDS);
+		TickScheduler.getInstance().runRepeating(this::sendPingPacket, 5, TimeUnit.SECONDS);
 	}
 
 	public long getPing() {
 		long currentTime = System.currentTimeMillis();
 
 		if (currentTime - lastUpdateTime >= 1500) {
-			displayedPing = lastPingResult;
+			currentPingResult = lastPingResult;
 			lastUpdateTime = currentTime;
 		}
 
-		return displayedPing;
+		return currentPingResult;
 	}
 
 	public float getTickRate() {
-		if (CLIENT == null || CLIENT.world == null || CLIENT.player == null) {
-			return 0;
-		}
-
-		if (System.currentTimeMillis() - timeGameJoined < 4000) {
-			return 20;
-		}
+		if (CLIENT == null || CLIENT.world == null || CLIENT.player == null) return 0;
+		if (System.currentTimeMillis() - timeGameJoined < 4000) return 20;
 
 		int numTicks = 0;
 		float sumTickRates = 0.0F;
@@ -65,9 +57,11 @@ public final class NetworkManager implements Manager {
 				numTicks++;
 			}
 		}
+
 		return sumTickRates / numTicks;
 	}
 
+	@EventHandler(event = "NetworkEvents.WORLD_TIME_UPDATE_PACKET")
 	private void onWorldTimeUpdatePacket() {
 		long now = System.currentTimeMillis();
 		float timeElapsed = (now - timeLastTimeUpdate) / 1000.0F;
@@ -76,18 +70,16 @@ public final class NetworkManager implements Manager {
 		timeLastTimeUpdate = now;
 	}
 
+	@EventHandler(event = "NetworkEvents.GAME_JOIN_PACKET")
 	private void onGameJoinPacket() {
 		Arrays.fill(tickRates, 0);
 		nextIndex = 0;
 		timeGameJoined = timeLastTimeUpdate = System.currentTimeMillis();
 	}
 
-	@Deprecated
 	private void sendPingPacket() {
-		if (CLIENT.player == null || CLIENT.getNetworkHandler() == null) {
-			return;
+		if (CLIENT.player != null && CLIENT.world != null && CLIENT.getNetworkHandler() != null) {
+			CLIENT.getNetworkHandler().sendPacket(new QueryPingC2SPacket(Util.getMeasuringTimeMs()));
 		}
-
-		CLIENT.getNetworkHandler().sendPacket(new QueryPingC2SPacket(Util.getMeasuringTimeMs()));
 	}
 }
