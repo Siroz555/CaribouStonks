8c9fdc0acdd136c0100f6f0ad699f48d3f605a57
Siroz555 <noreply.siroz555@gmail.com>
2025-07-29 10:11:05 +0200

    Backend: Add Pickup Item Event

---
+++ src/main/java/fr/siroz/cariboustonks/event/ItemEvents.java
@@ -0,0 +1,23 @@
+package fr.siroz.cariboustonks.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public final class ItemEvents {
+
+	/**
+	 * Called when a {@link ItemStack} is set in the player inventory slot.
+	 */
+	public static final Event<Pickup> PICKUP = EventFactory.createArrayBacked(Pickup.class, listeners -> (slot, stack) -> {
+		for (Pickup listener : listeners) {
+			listener.onPickup(slot, stack);
+		}
+	});
+
+	@FunctionalInterface
+	public interface Pickup {
+		void onPickup(int slot, @NotNull ItemStack stack);
+	}
+}
--- src/main/java/fr/siroz/cariboustonks/mixin/ScreenHandlerMixin.java
+++ src/main/java/fr/siroz/cariboustonks/mixin/ScreenHandlerMixin.java
@@ -1,6 +1,8 @@
 package fr.siroz.cariboustonks.mixin;
 
 import fr.siroz.cariboustonks.CaribouStonks;
+import fr.siroz.cariboustonks.core.skyblock.SkyBlockAPI;
+import fr.siroz.cariboustonks.event.ItemEvents;
 import fr.siroz.cariboustonks.manager.container.overlay.ContainerOverlayManager;
 import java.util.List;
 import net.minecraft.item.ItemStack;
@@ -16,6 +18,24 @@ public abstract class ScreenHandlerMixin {
 
 	@Unique
 	private final ContainerOverlayManager containerOverlay = CaribouStonks.managers().getManager(ContainerOverlayManager.class);
+
+	@Inject(method = "setStackInSlot", at = @At("HEAD"))
+	private void cariboustonks$onItemPickupEvent(int slot, int revision, ItemStack stack, CallbackInfo ci) {
+		if (SkyBlockAPI.isOnSkyBlock() && stack != null && !stack.isEmpty()) {
+			// < 9 useless, >= 45 not in the player inventory
+			int inventorySlot = slot;
+			if (inventorySlot < 9 || inventorySlot >= 45) {
+				return;
+			}
+
+			// Hotbar slots are at the end of the ids instead of at the start like in the inventory main stacks
+			if (inventorySlot >= 36) {
+				inventorySlot = inventorySlot - 36;
+			}
+
+			ItemEvents.PICKUP.invoker().onPickup(inventorySlot, stack);
+		}
+	}
 
 	@Inject(method = "setStackInSlot", at = @At("TAIL"))
 	private void cariboustonks$setStackInSlot(int slot, int revision, ItemStack stack, CallbackInfo ci) {
