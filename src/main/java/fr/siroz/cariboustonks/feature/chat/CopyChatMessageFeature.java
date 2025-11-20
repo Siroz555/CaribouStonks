4c3446a2307c094141f1cb3de71e3e9d42b8b91f
Siroz555 <noreply.siroz555@gmail.com>
2025-11-20 15:14:00 +0100

    Disable CopyChatMessageFeature for 1.21.11 pre1

--- src/main/java/fr/siroz/cariboustonks/feature/chat/CopyChatMessageFeature.java
+++ src/main/java/fr/siroz/cariboustonks/feature/chat/CopyChatMessageFeature.java
@@ -1,20 +1,15 @@
 package fr.siroz.cariboustonks.feature.chat;
 
-import fr.siroz.cariboustonks.CaribouStonks;
 import fr.siroz.cariboustonks.config.ConfigManager;
 import fr.siroz.cariboustonks.event.ChatEvents;
 import fr.siroz.cariboustonks.event.EventHandler;
 import fr.siroz.cariboustonks.feature.Feature;
-import fr.siroz.cariboustonks.mixin.accessors.ChatHudAccessor;
 import fr.siroz.cariboustonks.util.StonksUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.client.util.InputUtil;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
-
-import java.util.List;
-import java.util.stream.IntStream;
 
 public class CopyChatMessageFeature extends Feature {
 
@@ -45,40 +40,39 @@ public class CopyChatMessageFeature extends Feature {
 	}
 
 	private @Nullable ChatHudLine getMessageAt(double x, double y) {
-		try {
-			ChatHudAccessor accessor = (ChatHudAccessor) MinecraftClient.getInstance().inGameHud.getChatHud();
+		return null;
+//		try {
+//			ChatHudAccessor accessor = (ChatHudAccessor) MinecraftClient.getInstance().inGameHud.getChatHud();
 //			int lineSelected = accessor.invokeGetMessageLineIndex(
 //					accessor.invokeToChatLineX(x),
 //					accessor.invokeToChatLineY(y)
 //			);
-
-			// TODO
-			int lineSelected = 555;
-			if (lineSelected == -1) {
-				return null;
-			}
-
-			List<Integer> indexesOfEntryEnds = IntStream.range(0, accessor.getVisibleMessages().size())
-					.filter(index -> accessor.getVisibleMessages().get(index).endOfEntry())
-					.boxed()
-					.toList();
-
-			int indexOfMessageEntryEnd = indexesOfEntryEnds
-					.stream()
-					.filter(index -> index <= lineSelected)
-					.reduce((a, b) -> b)
-					.orElse(-1);
-
-			if (indexOfMessageEntryEnd == -1) {
-				return null;
-			}
-
-			int indexOfMessage = indexesOfEntryEnds.indexOf(indexOfMessageEntryEnd);
-			return accessor.getMessages().get(indexOfMessage);
-
-		} catch (Exception exception) { // index out of bounds.. = ???
-			CaribouStonks.LOGGER.error("[CopyChatMessageFeature] Error to get ChatHudLine", exception);
-			return null;
-		}
+//
+//			if (lineSelected == -1) {
+//				return null;
+//			}
+//
+//			List<Integer> indexesOfEntryEnds = IntStream.range(0, accessor.getVisibleMessages().size())
+//					.filter(index -> accessor.getVisibleMessages().get(index).endOfEntry())
+//					.boxed()
+//					.toList();
+//
+//			int indexOfMessageEntryEnd = indexesOfEntryEnds
+//					.stream()
+//					.filter(index -> index <= lineSelected)
+//					.reduce((a, b) -> b)
+//					.orElse(-1);
+//
+//			if (indexOfMessageEntryEnd == -1) {
+//				return null;
+//			}
+//
+//			int indexOfMessage = indexesOfEntryEnds.indexOf(indexOfMessageEntryEnd);
+//			return accessor.getMessages().get(indexOfMessage);
+//
+//		} catch (Exception exception) { // index out of bounds.. = ???
+//			CaribouStonks.LOGGER.error("[CopyChatMessageFeature] Error to get ChatHudLine", exception);
+//			return null;
+//		}
 	}
 }
