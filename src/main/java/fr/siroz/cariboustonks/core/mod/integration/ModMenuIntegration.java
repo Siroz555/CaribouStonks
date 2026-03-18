52a1eeedaec8c57d26b555099517e494d46eadbb
Siroz555 <noreply.siroz555@gmail.com>
2026-03-18 11:29:39 +0100

    Update Gradle script

--- src/main/java/fr/siroz/cariboustonks/core/mod/integration/ModMenuIntegration.java
+++ src/main/java/fr/siroz/cariboustonks/core/mod/integration/ModMenuIntegration.java
@@ -1,17 +1,17 @@
 package fr.siroz.cariboustonks.core.mod.integration;
 
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import fr.siroz.cariboustonks.config.ConfigManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.jspecify.annotations.NonNull;
 
 @Environment(EnvType.CLIENT)
-public final class ModMenuIntegration /*implements ModMenuApi*/ { // TODO :: 26.1
+public final class ModMenuIntegration implements ModMenuApi {
 
-	/*@Override
+	@Override
 	public @NonNull ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return ConfigManager::createConfigGUI;
-	}*/
+	}
 }
