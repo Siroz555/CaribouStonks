61ae43e0b833288d4031e73fc06e410c888811cd
Siroz555 <noreply.siroz555@gmail.com>
2026-05-04 18:12:55 +0200

    feat(core/skyblock): add SkyBlockTime

--- src/main/java/fr/siroz/cariboustonks/core/skyblock/SkyBlockTime.java
+++ src/main/java/fr/siroz/cariboustonks/core/skyblock/SkyBlockTime.java
@@ -7,20 +7,31 @@ public record SkyBlockTime(
 		int year,
 		int month,
 		int day,
-		int hour,
-		int minute,
-		int second
+		int hour
 ) implements Comparable<SkyBlockTime> {
 
-	public static final SkyBlockTime DEFAULT = new SkyBlockTime(1, 1, 1, 0, 0, 0);
+	public static final SkyBlockTime DEFAULT = new SkyBlockTime(1, 1, 1, 0);
+
+	private static final double HOUR_MILLIS = 50_000;
+	private static final double DAY_MILLIS = HOUR_MILLIS * 24;
+	private static final double MONTH_MILLIS = DAY_MILLIS * 31;
+	private static final double SEASON_MILLIS = MONTH_MILLIS * 3;
+	private static final double YEAR_MILLIS = SEASON_MILLIS * 4;
+
+	public static SkyBlockTime of(long millis) {
+		return new SkyBlockTime(
+				(int) (Math.floor(millis / YEAR_MILLIS) + 1), // 1-based
+				(int) (Math.floor(millis / MONTH_MILLIS) % 12), // 0-based
+				(int) (Math.floor(millis / DAY_MILLIS) % 31 + 1), // 1-based
+				(int) (Math.floor(millis / HOUR_MILLIS) % 24)  // 0-based
+		);
+	}
 
 	@Override
 	public int compareTo(SkyBlockTime other) {
 		if (year != other.year()) return Integer.compare(year, other.year());
 		if (month != other.month()) return Integer.compare(month, other.month());
 		if (day != other.day()) return Integer.compare(day, other.day());
-		if (hour != other.hour()) return Integer.compare(hour, other.hour());
-		if (minute != other.minute()) return Integer.compare(minute, other.minute());
-		return Integer.compare(second, other.second);
+		return Integer.compare(hour, other.hour());
 	}
 }
