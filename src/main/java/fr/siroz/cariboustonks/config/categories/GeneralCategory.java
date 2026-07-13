2d937aa9cc8b63d03cdbfa4a9c0f07bb11b36c6c
Siroz555 <noreply.siroz555@gmail.com>
2026-07-13 14:46:09 +0200

    feat(feature): remove Chocolate Factory Reminder

--- src/main/java/fr/siroz/cariboustonks/config/categories/GeneralCategory.java
+++ src/main/java/fr/siroz/cariboustonks/config/categories/GeneralCategory.java
@@ -307,15 +307,6 @@ public class GeneralCategory extends AbstractCategory {
 								.controller(this::createBooleanController)
 								.build())
 						.option(Option.<Boolean>createBuilder()
-								.name(Component.literal("Chocolate Factory | Max Chocolates Reminders"))
-								.description(OptionDescription.of(
-										Component.literal("Activate reminders when chocolate production is nearing its limit.")))
-								.binding(defaults.general.reminders.chocolateFactoryMaxChocolates,
-										() -> current.general.reminders.chocolateFactoryMaxChocolates,
-										newValue -> current.general.reminders.chocolateFactoryMaxChocolates = newValue)
-								.controller(this::createBooleanController)
-								.build())
-						.option(Option.<Boolean>createBuilder()
 								.name(Component.literal("Rift | Ubik's Cube Reminder"))
 								.description(OptionDescription.of(
 										Component.literal("Activate a reminder when your Ubik's Cube is ready for use.")))
