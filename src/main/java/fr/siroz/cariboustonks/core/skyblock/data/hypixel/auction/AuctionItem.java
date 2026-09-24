f4901166bf95439af42e63821a7b17b9f9c511cd
Siroz555 <noreply.siroz555@gmail.com>
2026-09-24 21:40:50 +0200

    refactor(core/skyblock): remove auction subdomain
    (cherry picked from commit 6c1a90c2376a99fbf8c889f9358845d07b77a45f)

--- src/main/java/fr/siroz/cariboustonks/core/skyblock/data/hypixel/auction/AuctionItem.java
+++
@@ -1,9 +0,0 @@
-package fr.siroz.cariboustonks.core.skyblock.data.hypixel.auction;
-
-import org.jspecify.annotations.NonNull;
-
-@Deprecated
-public record AuctionItem(
-		@NonNull String skyBlockId
-) {
-}
--- src/main/java/fr/siroz/cariboustonks/core/skyblock/data/hypixel/fetcher/AuctionFetcher.java
+++
@@ -1,5 +0,0 @@
-package fr.siroz.cariboustonks.core.skyblock.data.hypixel.fetcher;
-
-@Deprecated
-public final class AuctionFetcher {
-}
